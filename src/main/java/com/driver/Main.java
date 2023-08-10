56a4a0c25e0c09f3ae081b3d433d2a03240a252d
Prathamesh Khadake <pratham14785@gmail.com>
2023-08-10 16:19:21 +0530

    attempt-1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args){
+
+        RWOnly obj = new RWOnly();
+//        obj.name = "Pratham"; // name has a private access so we dont set the name and also dont access the name
+//        System.out.println(obj.name);
+        obj.setName("Pratham");
+        System.out.println(obj.getName());// by using setter and getter we can set and get the name
+    }
 }
